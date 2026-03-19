2c2d43872942bc6630cd9cbdcd103926770e647a
kcю <ksenia_mln@mail.ru>
2026-03-19 12:39:30 +0300

    Add login test in main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print('Hello from Developer A')
 print('This line added by A')
 print('This line added by B')
+
+# Testing login feature
+# result = login('admin', '123')
+# print('Login successful:', result)
